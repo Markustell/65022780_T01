011f49382c0c3c8c4bb096d07bb7896b57d8f705
Markustell <woraponklabsri7731@gmail.com>
2023-01-04 11:44:09 +0700

    add02

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-let randNum = Math.floor(Math.random() * 101);
+const randNum = Math.floor(Math.random() * 101);
+let guess_count = 1;
 
 function GN() {
+    alert(randNum)
     const Guess_Number = document.getElementById("G_N").value;
-
+   
+    if(Guess_Number == randNum) {
+        alert("Hello World " + randNum + " Count: " + guess_count);
+    }
+    else if(Guess_Number > randNum) {
+        guess_count++;
+        alert("มากว่า");
+    }
+    else {
+        guess_count++;
+        alert("น้อยกว่า");
+    }
 }
